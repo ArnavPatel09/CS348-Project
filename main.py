5fc2756742dc722ac14b0d1d0d82d41612ecb7eb
Arnav Patel <arnavpatel09@gmail.com>
2024-03-26 01:25:42 -0400

    created a function to get all open rooms

--- main.py
+++ main.py
@@ -71,19 +71,52 @@ def addUser(request):
     return (username, 200, headers)
 
 
+def getOpenRooms(request):
+    #bID = request.args.get('bID')
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+    if request.method == 'OPTIONS':
+        return ('', 204, headers)
+
+    # Connect to the MySQL database.
+    conn = mysql.connector.connect(host='35.238.112.0',
+                                    user='code',
+                                    password='pate1483',
+                                    #user='root',
+                                    database='users')
+    # Create a cursor.
+    cursor = conn.cursor(prepared=True)
+    # cursor.callproc('getAllRooms', (bID,))
+    # Execute the prepared statement.
+    cursor.execute(" SELECT * FROM rooms_table WHERE seats_available > 0",)
+
+    # Get the results of the stored procedure.
+    results = cursor.fetchall()
+    # Close the cursor and connection.
+    cursor.close()
+    conn.close()
+
+    # Return the results of the stored procedure.
+    return (results, 200, headers)
+
 
 # # TESTING FOR deleteUser
-request = type('Request', (object,), {'args': {'userID': 102}, 'method': 'POST'})()
+# request = type('Request', (object,), {'args': {'userID': 102}, 'method': 'POST'})()
 # print("starting")
-deleteUser(request)
+# deleteUser(request)
 # print("done")
 
 
-# Testing for addUser
-request = type('Request', (object,), {'args': {'firstname':'John', 'lastname':'Doe', 'email':'john.doe@example.com', 'username':'johndoe'}, 'method': 'POST'})()
-addUser(request)
-print("done")
+# # Testing for addUser
+# request = type('Request', (object,), {'args': {'firstname':'John', 'lastname':'Doe', 'email':'john.doe@example.com', 'username':'johndoe'}, 'method': 'POST'})()
+# addUser(request)
+# print("done")
 
-
+# Testing for getOpenRooms
+request = type('Request', (object,), {'args': {}, 'method': 'GET'})()
+print(getOpenRooms(request))
 
 
