520bf5c49b1d2f9b8ed9915649d12375393f4c4f
Arnav Patel <arnavpatel09@gmail.com>
2024-03-26 01:27:13 -0400

    created a function to get all rooms

--- main.py
+++ main.py
@@ -102,6 +102,38 @@ def getOpenRooms(request):
     # Return the results of the stored procedure.
     return (results, 200, headers)
 
+def getAllRooms(request):
+    #bID = request.args.get('bID')
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+    if request.method == 'OPTIONS':
+        return ('', 204, headers)
+
+    # Connect to the MySQL database.
+    conn = mysql.connector.connect(host='35.238.112.0',
+                                    user='code',
+                                    password='pate1483',
+                                    #user='root',
+                                    database='users')
+    # Create a cursor.
+    cursor = conn.cursor(prepared=True)
+    # cursor.callproc('getAllRooms', (bID,))
+    # Execute the prepared statement.
+    cursor.execute(" SELECT * FROM rooms_table",)
+
+    # Get the results of the stored procedure.
+    results = cursor.fetchall()
+    # Close the cursor and connection.
+    cursor.close()
+    conn.close()
+
+    # Return the results of the stored procedure.
+    return (results, 200, headers)
+
+
 
 # # TESTING FOR deleteUser
 # request = type('Request', (object,), {'args': {'userID': 102}, 'method': 'POST'})()
@@ -116,7 +148,11 @@ def getOpenRooms(request):
 # print("done")
 
 # Testing for getOpenRooms
-request = type('Request', (object,), {'args': {}, 'method': 'GET'})()
-print(getOpenRooms(request))
+# request = type('Request', (object,), {'args': {}, 'method': 'GET'})()
+# print(getOpenRooms(request))
+
+# # Testing for getAllRooms
+# request = type('Request', (object,), {'args': {}, 'method': 'GET'})()
+# print(getOpenRooms(request))
 
 
